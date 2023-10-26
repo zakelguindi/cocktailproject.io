540a42b7edcf3796b9e92584d3682539139429a1
Casey <caseyvu77@gmail.com>
2023-10-26 13:17:48 -0400

    okay

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ const getCocktails = async () => {
   const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
 
   try {
-    const link = await fetch(url); 
-    console.log(link); 
-    return await link.json();
+    const repsonse = await fetch(url); 
+    return await response.json();
   } catch(error) {
     console.log(error); 
   }
@@ -15,36 +14,21 @@ const getCocktails = async () => {
 const showCocktails = async () => {
   let cocktails = await getCocktails(); 
   let cocktailsSection = document.getElementById("main-content"); 
-  // cocktailsSection.append(getCocktailsItem(cocktails));
-  cocktails.drinks.forEach((cocktail) => {
+
+  cocktails.forEach((cocktail) => 
     cocktailsSection.append(getCocktailsItem(cocktail))
-  });
-};
+  );
+}
 
 const getCocktailsItem = (cocktail) => {
   const cocktailsSection = document.createElement("section");
   cocktailsSection.classList.add("cocktail");
 
   const a = document.createElement("a"); 
-  a.href = cocktail.website_url; 
+  a.href = cocktails.website_url; 
   cocktailsSection.append(a); 
 
   const h3 = document.createElement("h3");
-  h3.innerText = cocktail.strDrink; 
+  h3.innerText = cocktails.strDrink; 
   a.append(h3); 
-
-  const p = document.createElement("p"); 
-  p.textContent = `Instructions: ${cocktail.strInstructions}`; 
-  a.append(p); 
-
-  const img = document.createElement("img"); 
-  img.src = cocktail.strDrinkThumb; 
-  img.classList.add("img-format"); 
-  a.append(img); 
-
-  return cocktailsSection;
-
-
 };
-
-window.onload = () => showCocktails(); 
