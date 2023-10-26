ca1d4df0d71bd448a20bbe27fed1008bfcfa8466
Casey <caseyvu77@gmail.com>
2023-10-26 13:38:14 -0400

    accidentally overwrote stuff, but I fixed it

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
-
-
 const getCocktails = async () => {
   const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
 
   try {
-    const repsonse = await fetch(url); 
-    return await response.json();
-  } catch(error) {
-    console.log(error); 
+    const link = await fetch(url); 
+    console.log(link); 
+    return await link.json();
+  } catch (error) {
+    console.log(error);
   }
 };
 
 const showCocktails = async () => {
-  let cocktails = await getCocktails(); 
-  let cocktailsSection = document.getElementById("main-content"); 
+  let cocktails = await getCocktails();
+  let cocktailsSection = document.getElementById("main-content");
 
-  cocktails.forEach((cocktail) => 
+  cocktails.drinks.forEach((cocktail) => {
     cocktailsSection.append(getCocktailsItem(cocktail))
-  );
-}
+  });
+};
 
 const getCocktailsItem = (cocktail) => {
   const cocktailsSection = document.createElement("section");
   cocktailsSection.classList.add("cocktail");
 
-  const a = document.createElement("a"); 
-  a.href = cocktails.website_url; 
-  cocktailsSection.append(a); 
+  const a = document.createElement("a");
+  a.href = cocktail.website_url; 
+  cocktailsSection.append(a);
 
   const h3 = document.createElement("h3");
-  h3.innerText = cocktails.strDrink; 
-  a.append(h3); 
+  h3.innerText = cocktail.strDrink; 
+  a.append(h3);
+
+  const p = document.createElement("p"); 
+  p.textContent = `Instructions: ${cocktail.strInstructions}`; 
+  a.append(p); 
+
+  const img = document.createElement("img"); 
+  img.src = cocktail.strDrinkThumb; 
+  img.classList.add("img-format"); 
+  a.append(img); 
+
+  return cocktailsSection;
 };
+
+window.onload = () => showCocktails(); 
